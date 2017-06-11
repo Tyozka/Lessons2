b7a92248522afb11f94133404e77d14d3afbf055
4oc3p <4oc3p@mail.ru>
2017-06-11 13:08:19 +0300

    Lessons9 Interface

--- src/Lessons9/Cat.java
+++ src/Lessons9/Cat.java
@@ -1,9 +1,11 @@
 package Lessons9;
 
+import org.jetbrains.annotations.NotNull;
+
 /**
  * Created by User on 11.06.2017.
  */
-public class Cat extends Domestic implements Purrable {
+public class Cat extends Domestic implements Purrable, Comparable {
     public Cat(int id, int age, double weight, String color, String name, boolean isVactinated) {
         super(id, age, weight, color, name, isVactinated);
     }
@@ -15,5 +17,10 @@ public class Cat extends Domestic implements Purrable {
     @Override
     public void purr() {
         System.out.println("Myrr, Myrr, Myrr, Myrr | Cat");
+    }
+
+    @Override
+    public int compareTo(@NotNull Object o) {
+        return 0;
     }
 }
