f6b2d3f4309ecc8e75aaddb32ccfe5048cbab770
4oc3p <4oc3p@mail.ru>
2017-06-25 13:56:53 +0300

    Lessons13 Strategy pattern

---
+++ src/Lessonse13/App.java
@@ -0,0 +1,14 @@
+package Lessonse13;
+
+/**
+ * Created by User on 25.06.2017.
+ */
+public class App {
+    public static void main(String[] args) {
+        MyLinkedLIst list = new MyLinkedLIst();
+        list.add(2.5);
+
+        System.out.println();
+
+    }
+}
---
+++ src/Lessonse13/MyInteger.java
@@ -0,0 +1,23 @@
+package Lessonse13;
+
+/**
+ * Created by User on 25.06.2017.
+ */
+public class MyInteger {
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    private int data;
+
+    public MyInteger(int data){
+        this.data = data;
+    }
+
+
+
+}
